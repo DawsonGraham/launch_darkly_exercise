7fc29d9e7db93c803245bd3db9090e36634c34a7
Dawson Graham <hdawsong@gmail.com>
2019-01-26 17:16:13 -0800

    Install LDClient and confirm feature flag is properly passing true or false

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import LDClient from 'ldclient-js'
 import './App.css';
 
 class App extends Component {
@@ -8,22 +9,33 @@ class App extends Component {
     this.state = {
       showResults: false,
       featureFlags: {
-        defaultSuperBowlWinners: true
+        defaultSuperBowlWinner: null
       }
     }
   }
 
+  componentDidMount() {
+    const user = {
+      key: "Los Angeles"
+    }
+    this.ldclient = LDClient.initialize('5c4d02a9575ccb5233606fd7', user);
+    this.ldclient.on('ready', this.onLDReady.bind(this))
+  }
+
+  onLDReady() {
+    console.log('feature flag test',this.ldclient.variation('default-super-bowl-winner'))
+  }
 
   render() {
     let winnerLogo = undefined
     let winnerText = undefined
     
-    if(this.state.featureFlags.defaultSuperBowlWinners) {
+    if(this.state.featureFlags.defaultSuperBowlWinner) {
       if(this.state.showResults)  {
         winnerLogo = "http://evensondesign.com/wp-content/uploads/2013/04/patriots_a_logo_800x300.jpg"
         winnerText = "The New England Patriots!"
       }
-    } else if (!this.state.featureFlags.defaultSuperBowlWinners) {
+    } else if (!this.state.featureFlags.defaultSuperBowlWinner) {
         if(this.state.showResults) {
           winnerLogo = "https://mk0teamcolorcodtgc6i.kinstacdn.com/wp-content/uploads/2014/05/Los_Angeles_Rams_logo.jpg"
           winnerText = "The Los Angeles Rams!"
