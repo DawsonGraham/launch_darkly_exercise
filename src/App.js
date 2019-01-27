7d5a5b4a02b32f676d8edc771ae2af12bff301e6
Dawson Graham <hdawsong@gmail.com>
2019-01-26 16:56:45 -0800

    Initial click functionality in place

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showResults: false,
+      featureFlags: {
+        defaultSuperBowlWinners: true
+      }
+    }
+  }
+
+
   render() {
+    let winnerLogo = undefined
+    let winnerText = undefined
+    
+    if(this.state.featureFlags.defaultSuperBowlWinners) {
+      if(this.state.showResults)  {
+        winnerLogo = "http://evensondesign.com/wp-content/uploads/2013/04/patriots_a_logo_800x300.jpg"
+        winnerText = "The New England Patriots!"
+      }
+    } else if (!this.state.featureFlags.defaultSuperBowlWinners) {
+        if(this.state.showResults) {
+          winnerLogo = "https://mk0teamcolorcodtgc6i.kinstacdn.com/wp-content/uploads/2014/05/Los_Angeles_Rams_logo.jpg"
+          winnerText = "The Los Angeles Rams!"
+        }
+    }
+
     return (
       <div className="App">
           <h1>
             The winner of Super Bowl LIII will be...
-          </h1>    
+          </h1> 
+          <div>
+            <p className="Link" onClick={() => this.setState({ showResults: true})}> 
+              Click here to find out 
+            </p>
+          
+            <img src ={winnerLogo}/>
+            <h3> {winnerText} </h3>
+          </div>   
       </div>
     );
   }
